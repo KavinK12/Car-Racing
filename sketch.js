aeff65871219f386ed97e4459825cc4039b8ee84
KavinK12 <74074780+KavinK12@users.noreply.github.com>
2021-02-07 13:57:26 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 var back, gameState = 0, playerCount;
 var form, player, game, database;
+var allPlayers;
 
 function setup() {
   createCanvas(600,600);
   database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 
 }
 
 function draw() {
-  background(0);
+  background("red");
 }
